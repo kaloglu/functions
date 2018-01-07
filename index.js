0ed6e4cda1e1bbf2b8de4a44605244ccc533e960
kaloglu <cihan@kaloglu.com>
2018-01-08 01:27:01 +0300

    deletion trigger is ok
    when items delete on delete_queue , it creates automatically deleted object in deleted table.

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const functions = require('firebase-functions');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
 
 var db = functions.database;
-exports.deleteQueue = db.ref("/delete_queue/{tweetId}")
-	.onDelete(function (event) {
-		const tweet = event.data.val();
-		console.log("event", event)
-		console.log("event.data", event.data)
-		console.log("event.data.val", event.data.val())
-		console.log("tweetID", event.params.tweetId)
-		var tweetId = event.data.key;
-		return db.ref('/deleted/${tweetId}').set(tweet)
-	})
+exports.deleteQueue = db
+	.ref("/delete_queue/{tweetId}")
+    .onDelete(function (event) {
+        const tweet = event.data.previous.val();
+        console.log("event.data tweet", tweet);
+        console.log("tweetID", event.params.tweetId);
+        
+        var tweetId = event.params.tweetId;
+             
+	return admin.database()
+	.ref('/deleted/'+tweetId)
+	.set(tweet).then(()=>{
+		console.log('it is done');
+	});
+});
