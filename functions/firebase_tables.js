d54665838d6b143ad8cbaa86a2d0d5470b50107e
Cihan Kaloğlu <cihan@kaloglu.com>
2018-01-21 03:43:02 +0300

    functions trigger set for the send notification. Except, user object counts it always just check not check

--- functions/firebase_tables.js
+++ functions/firebase_tables.js
@@ -1,5 +1,6 @@
 module.exports = {
 	queue_table: "delete_queue",
 	deleted_table: "delete_list",
-	profiles_table: "profiles"
+	profiles_table: "profiles",
+	push_messages_table: "push_messages",
 }
